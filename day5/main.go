0d26c8e30053899e436eb30af2e8791b513eab02
Lasse Bjerre <lasse@lgb.dk>
2022-12-09 17:08:17 +0000

    More work on day5

--- day5/main.go
+++ day5/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strings"
 )
 
 type Crate struct {
@@ -11,9 +12,10 @@ type Crate struct {
 }
 
 type Stack struct {
-	Id     int
 	Crates []Crate
 }
+
+type Stacks map[int]*Stack
 
 func (s *Stack) Push(c Crate) {
 	s.Crates = append(s.Crates, c)
@@ -31,6 +33,8 @@ func (s *Stack) Move(quantity int, to *Stack) {
 }
 
 func main() {
+	stacks := make(Stacks)
+
 	data, err := os.Open("input_test.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -43,12 +47,29 @@ func main() {
 	for scanner.Scan() {
 		if len(scanner.Text()) == 0 {
 			stacksDone = true
+			continue
 		}
-		currentStack := 1
-		for i := 1; i < len(scanner.Text()); i += 4 {
-			log.Printf("Crate: %c", scanner.Text()[i])
-			log.Printf("Stack: %d", currentStack)
-			currentStack++
+		if !stacksDone {
+			currentStack := 1
+			for i := 1; i < len(scanner.Text()); i += 4 {
+				name := scanner.Text()[i]
+				if !strings.ContainsRune("ABCDEFGHIJKLMNOPQRSTUVWXYZ", rune(name)) {
+					continue
+				}
+				currentStack++
+				log.Printf("Crate: %c", name)
+				log.Printf("Stack: %d", currentStack)
+				create := Crate{Name: rune(name)}
+				if _, ok := stacks[currentStack]; !ok {
+					stacks[currentStack] = &Stack{}
+					log.Println("New stack made")
+				}
+				stacks[currentStack].Push(create)
+				currentStack++
+			}
+		} else {
+			log.Println(scanner.Text())
 		}
 	}
+	log.Printf("%c", stacks[1].Crates[0].Name)
 }
